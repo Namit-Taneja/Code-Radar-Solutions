a15bee354b1d00195f160c41d99de9dfc01eac92
Namit Taneja <namittaneja171@gmail.com>
2025-03-27 12:41:28 +0530

    Solution to Prime Number Checker in a Specific Range

--- Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
+++ Prime Number Checker in a Specific Range@@67ca7acd1fe08d7207c253b6/code.c
@@ -1,27 +1,53 @@
 // Your code here...
-void printPrimesInRange(int a,int b){
-int count_prime=0;
-    for(int i=a;i<=b;i++){
-        if(a==b){
-            int c=0;
-            for(int k=1;k<=a;k++){
-                if(a%k==0){
-                    c+=1;
-                }
-                else{
-                    continue;
-                }
-            }
-            if(c==2){
-                printf("%d ",a);
-            }
-            else{
-            printf("No prime numbers");}
-        }
-        else{
-        int count=0;
-        for(int j=2;j<=i;j++){
+// void printPrimesInRange(int a,int b){
+// int count_prime=0;
+//     for(int i=a;i<=b;i++){
+//         if(a==b){
+//             int c=0;
+//             for(int k=1;k<=a;k++){
+//                 if(a%k==0){
+//                     c+=1;
+//                 }
+//                 else{
+//                     continue;
+//                 }
+//             }
+//             if(c==2){
+//                 printf("%d ",a);
+//             }
+//             else{
+//             printf("No prime numbers");}
+//         }
+//         else{
+//         int count=0;
+//         for(int j=2;j<=i;j++){
             
+//             if(i%j==0){
+//                 count+=1;
+//             }
+//             else{
+//                 continue;
+//             }
+//         }
+//         if(count==1){
+//             count_prime+=1;
+//             printf("%d ",i);
+//         }
+//         else{
+//             continue;
+//         }
+//         if(count_prime==0){
+//             printf("No prime numbers");
+//         }
+//         }
+//     }
+// }
+
+
+int printPrimesInRange(int a,int b){
+    for(int i=a;i<b;i++){
+        int count=0;
+        for(int j=1;j<=i;j++){
             if(i%j==0){
                 count+=1;
             }
@@ -29,16 +55,8 @@ int count_prime=0;
                 continue;
             }
         }
-        if(count==1){
-            count_prime+=1;
-            printf("%d ",i);
-        }
-        else{
-            continue;
-        }
-        if(count_prime==0){
-            printf("No prime numbers");
-        }
+        if(count==2){
+            return i;
         }
     }
 }
